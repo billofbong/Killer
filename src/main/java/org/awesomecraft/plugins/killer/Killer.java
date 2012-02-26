17f34c23cbf38f36342ab538321ab73cef54ada4
Will Wright <willjwright@mac.com>
2012-02-26 10:00:35 -0800

    Added command, fixed crappy boolean

--- src/main/java/org/awesomecraft/plugins/killer/Killer.java
+++ src/main/java/org/awesomecraft/plugins/killer/Killer.java
@@ -1,17 +1,40 @@
 package org.awesomecraft.plugins.killer;
 
+import java.util.HashSet;
+import java.util.Set;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Killer extends JavaPlugin implements Listener {
+    
+    public Set<Player> killEnabled = new HashSet<Player>(); 
     public void onDisable() {
-        // TODO: Place any custom disable code here.
     }
 
     public void onEnable() {
+        getCommand("blazekill").setExecutor(new CommandExecutor() {
+        public boolean onCommand(CommandSender cs, Command cmnd, String name, String[] args) {
+        Player sender = (Player) cs;
+        if(sender.hasPermission("killer.kill")) {
+            if(!killEnabled.contains(sender)) {
+            killEnabled.add(sender);
+            sender.sendMessage(ChatColor.GREEN + "Killer enabled.");
+            } else {
+                killEnabled.remove(sender);
+                sender.sendMessage(ChatColor.GREEN + "Killer disabled.");
+            }
+        }
+        return true;
+    }
+        });
         getServer().getPluginManager().registerEvents(this, this);
     }
 
@@ -19,7 +42,7 @@ public class Killer extends JavaPlugin implements Listener {
     public void onPlayerEntityInteract(PlayerInteractEntityEvent event) {
         if(event.getPlayer().getItemInHand().getType().equals(Material.BLAZE_ROD) 
                 && event.getPlayer().isSneaking()
-                && event.getPlayer().hasPermission("killer.kill")) {
+                && killEnabled.contains(event.getPlayer())) {
             event.getRightClicked().setFallDistance(1000000000000000000F);
         }
     }
